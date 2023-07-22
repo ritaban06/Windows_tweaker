fcc7c160fd5714252296e097b2429fd127378d5d
Ritaban Ghosh <ghoshritaban2006@gmail.com>
2023-07-22 18:37:24 +0530

    2nd commit

--- welcome.py
+++ welcome.py
@@ -26,7 +26,7 @@ def create_restore_point():
         print("Failed to create System Restore Point.")
 
 def print_welcome_message():
-    print("Welcome to Windows Tweaking Program!")
+    print("Welcome to Windows Tweaking Program!\n")
 
     if not is_system_restore_enabled():
         print("Warning: System Restore is currently disabled.")
@@ -42,5 +42,23 @@ def print_welcome_message():
 if __name__ == "__main__":
     print_welcome_message()
 
-    # After the System Restore Point is created, proceed to the menu
-    import menu
+    # After the System Restore Point is created, show the main menu
+    while True:
+        print("\nPlease select a menu:")
+        print("1. All Features")
+        print("2. Only for Windows 11")
+        print("3. Exit")
+        
+        choice = input("Enter the number corresponding to the menu: ")
+        
+        if choice == "1":
+            import all_features_menu
+            break
+        elif choice == "2":
+            import windows_11_features_menu
+            break
+        elif choice == "3":
+            print("Exiting...")
+            break
+        else:
+            print("Invalid choice. Please enter a valid number.")
